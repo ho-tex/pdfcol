6a6a800aea2704942744ebd98597b224886581ba
David Carlisle <d.p.carlisle@gmail.com>
2022-09-21 20:17:34 +0100

    copied from oberdiek

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pdfcol
-pdfcol packge
+
+* pdfcol: macros for setting and maintaining new color stacks;
+
+
+Since version 1.40 pdfTeX supports color stacks.
+The driver file pdftex.def for package color
+defines and uses a main color stack since version v0.04b.
+Package pdfcol is intended for package writers.
+It defines macros for setting and maintaining new color stacks.
+
+
---
+++ build.lua
@@ -0,0 +1,58 @@
+#!/usr/bin/env texlua
+
+
+module = "pdfcol"
+
+
+
+textfiles = {"README.md"}
+unpackfiles = {'pdfcol.dtx'}
+
+packtdszip  = true
+
+maxprintline=10000
+checkruns = 2
+
+tdslocations={
+"doc/latex/pdfcol/pdfcol-example.tex",
+"doc/latex/pdfcol/pdfcol.pdf",
+"source/latex/pdfcol/pdfcol.dtx",
+"tex/generic/pdfcol/pdfcol.sty",
+}
+
+
+checkconfigs = {"build","config-noxetex","config-luatex"}
+
+function update_tag(file,content,tagname,tagdate)
+
+local tagpattern="(%d%d%d%d[-/]%d%d[-/]%d%d) v(%d+[.])(%d+)"
+local oldv,newv
+if tagname == 'auto' then
+  local i,j,olddate,a,b
+  i,j,olddate,a,b= string.find(content, tagpattern)
+  if i == nil then
+    print('OLD TAG NOT FOUND')
+    return content
+  else
+    print ('FOUND: ' .. olddate .. ' v' .. a .. b )
+    oldv = olddate .. ' v' .. a .. b
+    newv = tagdate .. ' v'  .. a .. math.floor(b + 1)
+    print('USING OLD TAG: ' .. oldv)
+    print('USING NEW TAG: ' .. newv)
+    local oldpattern = string.gsub(oldv,"[-/]", "[-/]")
+    content=string.gsub(content,"{Version}{" .. oldpattern,'##OLDV##')
+    content=string.gsub(content,oldpattern,newv)
+    content=string.gsub(content,'##OLDV##',"{Version}{" .. oldv)
+    content=string.gsub(content,'%-%d%d%d%d Oberdiek Package','-' .. os.date("%Y") .. " Oberdiek Package")
+    content = string.gsub(content,
+        '%% \\end{History}',
+	'%%   \\begin{Version}{' .. newv .. '}\n%%   \\item Updated\n%%   \\end{Version}\n%% \\end{History}')
+    return content
+  end
+else
+  error("only automatic tagging supported")
+end
+
+end
+
+
